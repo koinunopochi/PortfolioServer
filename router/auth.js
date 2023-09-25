38bd40e3bb05216ec02a2a8323748f770f158adc
okayama <okapi771205a@gmail.com>
2023-09-26 02:21:34 +0900

    loginに関して記述

--- router/auth.js
+++ router/auth.js
@@ -1,17 +1,43 @@
 const express = require('express');
-const MyCustomError = require('../lib/custom_error');
+const { MyCustomError } = require('../lib/custom_error');
+const {
+  ValidationEmail,
+  ValidationPassword,
+  ValidationParams,
+} = require('../lib/validate');
+const { getUserInfo } = require('../controller/user');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    ValidationParams(req.body, ['email', 'password']);
+    ValidationEmail(email);
+    ValidationPassword(password);
 
-router.post('/login', (req, res) => {
-  // Login code here
-  console.log(req.body);
-  if(!req.body.username || !req.body.password) {
-    throw new MyCustomError('BadRequest', 'Username or password is missing', 400);
+    console.log(email, password);
+
+    const user = await getUserInfo({ email });
+
+    console.log(user);
+
+    if (!user) {
+      throw new MyCustomError('NotExistUser', 'not exist user', 400);
+    }
+    const is_password = await bcrypt.compare(password, user.password);
+    if (!is_password) {
+      throw new MyCustomError('InvalidPassword', 'invalid password', 400);
+    }
+    if (!user.is_verify) {
+      throw new MyCustomError('InvalidUser', 'invalid user', 400);
+    }
+
+    res.json({ message: 'Login successful' });
+  } catch (err) {
+    next(err);
   }
-
-  res.json({ message: 'Login successful' });
 });
 
 exports.router = router;
