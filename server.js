21967b9df287284c329f2359d557924938d08759
okayama <okapi771205a@gmail.com>
2023-09-25 14:41:38 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+
+app.use(cors());
+spp.use(bodyParser.json());
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
