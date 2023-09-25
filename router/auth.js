870b80b1fe69831df2836828e14d9cd55f5ee001
okayama <okapi771205a@gmail.com>
2023-09-26 02:31:34 +0900

    ぬけのインポート

--- router/auth.js
+++ router/auth.js
@@ -5,10 +5,15 @@ const {
   ValidationPassword,
   ValidationParams,
 } = require('../lib/validate');
-const { getUserInfo } = require('../controller/user');
+const { getUserInfo, insertRefreshToken, getRefreshToken, deleteRefreshToken } = require('../controller/user');
 const router = express.Router();
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { logger } = require('../lib/logger');
+
+require('dotenv').config();
+const { SECRET_KEY, REFRESH_SECRET_KEY } = process.env;
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -33,6 +38,38 @@ router.post('/login', async (req, res, next) => {
     if (!user.is_verify) {
       throw new MyCustomError('InvalidUser', 'invalid user', 400);
     }
+
+    // リフレッシュトークンがあるか確認
+    const is_refresh_token = await getRefreshToken(email);
+    logger.debug(is_refresh_token);
+    // リフレッシュトークンがある場合は削除
+    if (is_refresh_token) {
+      await deleteRefreshToken(email);
+    }
+
+    // JWTを発行
+    const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: '15m' });
+    const refreshToken = jwt.sign({ email }, REFRESH_SECRET_KEY, {
+      expiresIn: '7d',
+    });
+    // リフレッシュトークンをDBに保存
+    await insertRefreshToken(email, refreshToken);
+
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      path: '/',
+      domain: 'localhost',
+    });
+    res.cookie('authToken', token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      path: '/',
+      domain: 'localhost',
+    });
 
     res.json({ message: 'Login successful' });
   } catch (err) {
