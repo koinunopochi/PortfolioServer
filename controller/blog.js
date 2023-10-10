977c20fef42c58f485384f50142df8612aa5b4ab
okayama <okapi771205a@gmail.com>
2023-10-10 09:54:22 +0900

    update_date,dateの追記

--- controller/blog.js
+++ controller/blog.js
@@ -7,7 +7,11 @@ const insertBlog = async (data) => {
     title: data.title,
     content: data.content,
     overview: data.overview,
+    category: data.category,
+    tags: data.tags,
+    role: data.role,
     date: data.date || new Date(),
+    update_date: data.update_date || new Date(),
   });
   return result;
 };
@@ -20,7 +24,11 @@ const updateBlog = async (id, data) => {
       $set: {
         title: data.title,
         content: data.content,
+        category: data.category,
+        tags: data.tags,
+        role: data.role,
         overview: data.overview,
+        update_date: new Date(),
       },
     }
   );
@@ -29,10 +37,14 @@ const updateBlog = async (id, data) => {
 
 const getBlogs = async () => {
   const db = getDb();
-  return db.collection('blogs').find().toArray();
+  return db
+    .collection('blogs')
+    .find({ role: { $ne: 'draft' } })
+    .toArray();
 };
 
-const getBloOverviews = async () => {
+
+const getBlogOverviews = async () => {
   const db = getDb();
   const overviews = await db
     .collection('blogs')
@@ -43,18 +55,52 @@ const getBloOverviews = async () => {
           _id: 1, // 明示的に_idを含める。
           title: 1,
           overview: 1,
+          category:1,
+          tags:1,
+          date: 1,
+          update_date: 1,
         },
       }
     )
     .toArray();
   return overviews;
 };
+const getBlogOverviewsIgnoreDraft = async () => {
+  const db = getDb();
+  const overviews = await db
+    .collection('blogs')
+    .find(
+      { role: { $ne: 'draft' } },
+      {
+        projection: {
+          _id: 1,
+          title: 1,
+          overview: 1,
+          category: 1,
+          tags: 1,
+          date: 1,
+          update_date: 1,
+        },
+      }
+    )
+    .toArray();
+  return overviews;
+};
+exports.getBlogOverviewsIgnoreDraft = getBlogOverviewsIgnoreDraft;
 
 
 const getBlog = async (id) => {
   const db = getDb();
   return db.collection('blogs').findOne({ _id: new ObjectId(id) });
 }
+const getBlogIgnoreDraft = async (id) => {
+  const db = getDb();
+  return db
+    .collection('blogs')
+    .findOne({ _id: new ObjectId(id), role: { $ne: 'draft' } });
+};
+exports.getBlogIgnoreDraft = getBlogIgnoreDraft;
+
 
 const deleteBlog = async (id) => {
   const db = getDb();
@@ -62,4 +108,4 @@ const deleteBlog = async (id) => {
   return result.deletedCount === 1;
 };
 
-module.exports = { insertBlog, getBlogs, deleteBlog, getBlog , getBloOverviews, updateBlog};
+module.exports = { insertBlog, getBlogs, deleteBlog, getBlog , getBlogOverviews, updateBlog};
