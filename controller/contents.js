f7aa447304f07058de5a2244c9365d292ed3976f
okayama <okapi771205a@gmail.com>
2023-10-15 23:38:37 +0900

    必要のないファイルの削除

--- controller/contents.js
+++
@@ -1,9 +0,0 @@
-const {getDb} = require('../lib/mongo');
-
-const insertContents = async (data) => {
-  const db = getDb();
-  const collection = db.collection('contents');
-  const result = await collection.insertOne(data);
-  return result;
-};
-exports.insertContents = insertContents;
