7cb3c116eb6590c9306f7a41893a7c073589a93d
okayama <okapi771205a@gmail.com>
2023-10-15 23:31:03 +0900

    呼び出し法の変更

--- controller/access_log.js
+++ controller/access_log.js
@@ -1,34 +1,45 @@
 const { getDb } = require('../lib/mongo');
-const { ObjectId } = require('mongodb');
+// const { ObjectId } = require('mongodb');
+const DbOperations = require('./DbOperations');
 
-const insertAccessLog = async (data) => {
-  const db = getDb();
-  const result = await db.collection('access_logs').insertOne({
-    ip: data.ip,
-    method: data.method,
-    url: data.url,
-    time: data.date || new Date(),
-  });
-  return result;
+const accessCollection = new DbOperations('access_logs');
+
+// const insertAccessLog = async (data) => {
+//   const db = getDb();
+//   const result = await db.collection('access_logs').insertOne({
+//     ip: data.ip,
+//     method: data.method,
+//     url: data.url,
+//     time: data.date || new Date(),
+//   });
+//   return result;
+// };
+const insertAccessLog = async (ip, method, url, time) => {
+  const time_ = time || new Date();
+  return await accessCollection.insert({ ip, method, url, time: time_ });
 };
 
 exports.insertAccessLog = insertAccessLog;
 
-const getAccessLogs = async (startTime, endTime) => {
-  const db = getDb();
-  const start = new Date(startTime);
-  const end = new Date(endTime);
+// const getAccessLogs = async (startTime, endTime) => {
+//   const db = getDb();
+//   const start = new Date(startTime);
+//   const end = new Date(endTime);
 
-  // MongoDBの日時のクエリを用いて、指定された期間のログを取得
-  return db
-    .collection('access_logs')
-    .find({
-      time: {
-        $gte: start,
-        $lte: end,
-      },
-    })
-    .toArray();
+//   // MongoDBの日時のクエリを用いて、指定された期間のログを取得
+//   return db
+//     .collection('access_logs')
+//     .find({
+//       time: {
+//         $gte: start,
+//         $lte: end,
+//       },
+//     })
+//     .toArray();
+// };
+const getAccessLogs = async(start_time, end_time) => {
+    const start = new Date(start_time);
+    const end = new Date(end_time);
+  return await accessCollection.find({ time: { $gte: start, $lte: end } });
 };
-
 exports.getAccessLogs = getAccessLogs;
