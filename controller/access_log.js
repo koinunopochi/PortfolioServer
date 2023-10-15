b900d3641ad5d35adf0a3ed5a8fcea8ae7b0a04e
okayama <okapi771205a@gmail.com>
2023-10-15 23:35:07 +0900

    コメントの追加

--- controller/access_log.js
+++ controller/access_log.js
@@ -1,42 +1,27 @@
-const { getDb } = require('../lib/mongo');
-// const { ObjectId } = require('mongodb');
 const DbOperations = require('./DbOperations');
 
 const accessCollection = new DbOperations('access_logs');
 
-// const insertAccessLog = async (data) => {
-//   const db = getDb();
-//   const result = await db.collection('access_logs').insertOne({
-//     ip: data.ip,
-//     method: data.method,
-//     url: data.url,
-//     time: data.date || new Date(),
-//   });
-//   return result;
-// };
+/**
+ * アクセスされたログをDBに保存する
+ * @param {string} ip ip address
+ * @param {string} method POST, GET, PUT, DELETE 
+ * @param {url} url アクセスされたURL
+ * @param {Date} time アクセス時間
+ * @returns 
+ */
 const insertAccessLog = async (ip, method, url, time) => {
   const time_ = time || new Date();
   return await accessCollection.insert({ ip, method, url, time: time_ });
 };
-
 exports.insertAccessLog = insertAccessLog;
 
-// const getAccessLogs = async (startTime, endTime) => {
-//   const db = getDb();
-//   const start = new Date(startTime);
-//   const end = new Date(endTime);
-
-//   // MongoDBの日時のクエリを用いて、指定された期間のログを取得
-//   return db
-//     .collection('access_logs')
-//     .find({
-//       time: {
-//         $gte: start,
-//         $lte: end,
-//       },
-//     })
-//     .toArray();
-// };
+/**
+ * 時間によってアクセスされたログを取得する
+ * @param {Date} start_time 取得する時間の開始
+ * @param {Date} end_time 取得する時間の終了
+ * @returns 
+ */
 const getAccessLogs = async(start_time, end_time) => {
     const start = new Date(start_time);
     const end = new Date(end_time);
