44ddb63e66fa5ad87b2a7a22d259c47f0b000ddd
okayama <okapi771205a@gmail.com>
2023-10-16 22:32:30 +0900

    JsDocの追加

--- router/blog.js
+++ router/blog.js
@@ -13,6 +13,17 @@ const { MyCustomError } = require('../lib/CustomError');
 const { admin_route } = require('../utils/adminRoute');
 
 // 未認証ルート
+
+/**
+ * すべてのブログの概要を取得するエンドポイント。
+ * 
+ * @route GET /overviews
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Object[]} JSON - すべてのブログの概要のリスト。
+ * @throws {Error} データベースエラーやその他の問題が発生した場合にエラーをスローします。
+ */
 router.get('/overviews', async (req, res, next) => {
   try {
     const overviews = await getBlogOverviews();
@@ -22,6 +33,17 @@ router.get('/overviews', async (req, res, next) => {
   }
 });
 
+
+/**
+ * 指定したIDのブログの情報を取得するエンドポイント。
+ * 
+ * @route GET /:blogId
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Object} JSON - 指定したIDのブログの情報。
+ * @throws {MyCustomError} ブログが見つからない場合、または無効なブログIDが指定された場合にエラーをスローします。
+ */
 router.get('/:blogId', async (req, res, next) => {
   try {
     const blog = await getBlog(req.params.blogId);
@@ -39,24 +61,50 @@ router.get('/:blogId', async (req, res, next) => {
   }
 });
 
-// とりあえずは、問題がない
-// 全部のブログの情報をとるが、あまりよくない
-// overviewsを使うべき
-// TODO:フロントのぷろじぇくとインデックスの部分で誤って使用しているため、変更する
+
+/**
+ * すべてのブログの情報を取得するエンドポイント。
+ * 注意: 実際の運用においては、すべての情報を取得するのではなく、概要だけを取得すべきである。
+ * しかし、現在のフロントエンドプロジェクトのインデックス部分で誤ってこのエンドポイントを使用しているため、
+ * その部分の修正が必要です。
+ * 
+ * @route GET /
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Array<Object>} JSON - 取得したすべてのブログの情報。
+ * 
+ * TODO: フロントエンドプロジェクトのインデックス部分での使用を修正する。
+ */
 router.get('/', async (req, res, next) => {
   try {
-    const blogs = await getAllBlogs(); // 更新
-    // const blogs = await getBlogOverviews(); // 更新
+    const blogs = await getAllBlogs();
+    // const blogs = await getBlogOverviews();
     res.json(blogs);
   } catch (err) {
     next(err);
   }
 });
 
-// 認証ルート
+
+// 認証ルート　＆＆　バリデーションは自身しか使わないため、不要
 router.use(admin_route);
 
-// 最低限追加が可能、バリデーションは自身しか使わないため、不要
+/**
+ * 新しいブログを作成するエンドポイント。
+ * 
+ * @route POST /
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} req.body - リクエストボディ。
+ * @param {string} req.body.title - ブログのタイトル。
+ * @param {string} req.body.content - ブログのコンテンツ。
+ * @param {string} req.body.overview - ブログの概要。
+ * @param {string} req.body.category - ブログのカテゴリ。
+ * @param {Array<string>} req.body.tags - ブログのタグのリスト。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Object} JSON - 作成が成功した場合のメッセージおよびブログの情報。
+ */
 router.post('/', async (req, res, next) => {
   try {
     const { title, content, overview, category, tags } = req.body;
@@ -78,6 +126,25 @@ router.post('/', async (req, res, next) => {
     next(err);
   }
 });
+
+
+/**
+ * 指定されたIDのブログを更新するエンドポイント。
+ * 
+ * @route PUT /:blogId
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} req.params - ルートのパラメータ。
+ * @param {string} req.params.blogId - 更新するブログのID。
+ * @param {Object} req.body - リクエストボディ。
+ * @param {string} req.body.title - ブログのタイトル。
+ * @param {string} req.body.content - ブログのコンテンツ。
+ * @param {string} req.body.overview - ブログの概要。
+ * @param {string} req.body.category - ブログのカテゴリ。
+ * @param {Array<string>} req.body.tags - ブログのタグのリスト。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Object} JSON - 更新が成功した場合のメッセージおよびブログの情報。
+ */
 router.put('/:blogId', async (req, res, next) => {
   try {
     const { title, content, overview, category, tags } = req.body;
@@ -93,7 +160,7 @@ router.put('/:blogId', async (req, res, next) => {
     logger.debug(savedBlog);
     logger.info('id:' + savedBlog.insertedId);
     res.json({
-      message: 'Blog created',
+      message: 'Blog updated',
       blog: { title, overview, content },
       id: savedBlog.insertedId,
     });
@@ -102,10 +169,21 @@ router.put('/:blogId', async (req, res, next) => {
   }
 });
 
-// とりあえずは、問題がない
+
+/**
+ * 指定されたIDのブログを削除するエンドポイント。
+ * 
+ * @route DELETE /:blogId
+ * @param {Object} req - Expressのリクエストオブジェクト。
+ * @param {Object} req.params - ルートのパラメータ。
+ * @param {string} req.params.blogId - 削除するブログのID。
+ * @param {Object} res - Expressのレスポンスオブジェクト。
+ * @param {function} next - Expressのミドルウェア関数。
+ * @returns {Object} JSON - 削除が成功した場合は成功メッセージ、失敗した場合はエラーメッセージ。
+ */
 router.delete('/:blogId', async (req, res, next) => {
   try {
-    const isDeleted = await deleteBlog(req.params.blogId); // 更新
+    const isDeleted = await deleteBlog(req.params.blogId);
     logger.debug(isDeleted);
     if (isDeleted) {
       logger.info('deleted blog' + req.params.blogId);
@@ -117,5 +195,6 @@ router.delete('/:blogId', async (req, res, next) => {
     next(err);
   }
 });
+
 
 exports.router = router;
