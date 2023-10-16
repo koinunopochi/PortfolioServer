3d0408199eb2135facc6058fe055d8dda4fcaf2f
okayama <okapi771205a@gmail.com>
2023-10-16 14:49:31 +0900

    ドキュメントの作成

--- utils/admin_route.js
+++ utils/admin_route.js
@@ -1,22 +1,29 @@
-const jwt = require('jsonwebtoken');
 const { getUserAll } = require('../controller/user');
 const { MyCustomError } = require('./custom_error');
 const { logger } = require('../lib/logger');
+const { decodeItem } = require('./jwt');
 
+/**
+ * 管理者のみがアクセスを許可されたルートを保護するためのミドルウェア。
+ *
+ * @function
+ * @async
+ * @param {Object} req - Expressのリクエストオブジェクト
+ * @param {Object} res - Expressのレスポンスオブジェクト
+ * @param {function} next - 次のミドルウェアまたはルートハンドラーへのコールバック関数
+ * @throws 何らかのエラーが発生した場合、エラーハンドリングミドルウェアにエラーオブジェクトが渡されます
+ */
 const protect = async (req, res, next) => {
-  // jwtからusernameを取得する
-  // usernameからroleを取得する
-  // roleがadminか確認する
   try {
     const cookie = req.cookies.authToken;
+    // 認証トークンが存在しない場合はエラーを投げる
     if (!cookie) {
       throw new MyCustomError('InvalidUser', 'invalid user', 400);
     }
-    const decoded = jwt.verify(cookie, process.env.SECRET_KEY);
-    const username = decoded.username;
-    const userInfo = await getUserAll(username);
-    logger.debug(userInfo.role);
-    if (userInfo.role !== 'admin') {
+    // 管理者かどうかを確認する
+    const is_admin = await isAdmin(req);
+    // ユーザーが管理者でない場合はエラーを投げる
+    if (is_admin !== true) {
       throw new MyCustomError('InvalidUser', 'invalid user', 400);
     }
     next();
@@ -29,22 +36,38 @@ const protect = async (req, res, next) => {
 };
 exports.admin_route = protect;
 
+/**
+ * 管理者であるかどうかを判断する関数。
+ *
+ * @function
+ * @async
+ * @param {Object} req - Expressのリクエストオブジェクト
+ * @returns {boolean} 管理者であればtrue、それ以外はfalse
+ * @throws 何らかのエラーが発生した場合はエラーがスローされます
+ */
 const isAdmin = async (req) => {
   try {
     const cookie = req.cookies.authToken;
+    // 認証トークンが存在しない場合はエラーを投げる
     if (!cookie) {
       throw new MyCustomError('InvalidUser', 'invalid user', 400);
     }
-    const decoded = jwt.verify(cookie, process.env.SECRET_KEY);
-    const username = decoded.username;
+
+    // トークンからユーザー名をデコードする
+    const username = decodeItem(cookie, 'username');
+
+    // ユーザー情報を取得
     const userInfo = await getUserAll(username);
     logger.debug(userInfo.role);
-    if (userInfo.role !== 'admin') {
-      return false;
+
+    // ユーザーが管理者であればtrueを返す
+    if (userInfo.role === 'admin') {
+      return true;
     }
-    return true;
+    return false;
   } catch (error) {
     throw error;
   }
 };
 exports.isAdmin = isAdmin;
+
