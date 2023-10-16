8c36d4a72d0340190c3f927437e2a3edf9b8363d
okayama <okapi771205a@gmail.com>
2023-10-16 23:59:53 +0900

    重複部分を削除

--- server.js
+++ server.js
@@ -31,10 +31,6 @@ app.use(cookieParser());
 
 const server_port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  logger.info(req.method + ' ' + req.url);
-  next();
-});
 app.use('/auth', authRouter);
 app.use('/blog', blogRouter);
 app.use('/contact', contactRouter);
