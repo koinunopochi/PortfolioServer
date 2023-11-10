19f1b24f78be14ee5605a1e382773f54b63f1cbd
okayama <okapi771205a@gmail.com>
2023-11-10 11:22:54 +0900

    Add Refresh Token model and operations

---
+++ models/refreshToken.js
@@ -0,0 +1,47 @@
+// const { logger } = require('../lib/logger');
+
+const DbOperations = require('../lib/DbOperations');
+
+const refreshTokenOperations = new DbOperations('refresh_tokens');
+
+/**
+ * 指定したユーザーにリフレッシュトークンを付与する関数
+ * @param {string} username ユーザー名
+ * @param {string} refresh_token リフレッシュ用のトークン
+ * @returns insert結果を返す
+ */
+const insertRefreshToken = async (username, refresh_token) => {
+  return await refreshTokenOperations.insert({
+    username,
+    refresh_token,
+  });
+};
+exports.insertRefreshToken = insertRefreshToken;
+
+
+/**
+ * リフレッシュトークンの取得
+ * @param {string} username
+ * @returns 取得した結果を返す
+ */
+const getRefreshToken = async (username) => {
+  return await refreshTokenOperations.findOne(
+    { username },
+    {
+      _id: 0,
+      refresh_token: 1,
+    }
+  );
+};
+exports.getRefreshToken = getRefreshToken;
+
+
+/**
+ * リフレッシュトークンの削除
+ * @param {string} username
+ * @returns 結果を返す
+ */
+const deleteRefreshToken = async (username) => {
+  return await refreshTokenOperations.delete({ username });
+};
+exports.deleteRefreshToken = deleteRefreshToken;
